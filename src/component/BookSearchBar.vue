9909ff603826a86bb4b7a5044c6fe9a582422788
Jen Hoang <jenhoang304@gmail.com>
2023-11-04 13:59:44 -0700

    Create BookSearchBar component

---
+++ src/component/BookSearchBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+import BaseInput from '@/component/BaseInput.vue';
+import BaseButton from '@/component/BaseButton.vue';
+import { ref } from 'vue';
+// define refs
+const bookSearchBarRef = ref('');
+</script>
+<template>
+  <base-input prefix-icon="search" placeholder="Search book" clearable v-model="bookSearchBarRef">
+    <template #inner-right>
+      <base-button>Search</base-button>
+    </template>
+  </base-input>
+</template>
--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,18 +1,13 @@
 <script setup>
 // import components
 import MainLayout from './layout/MainLayout.vue';
-import BaseInput from '@/component/BaseInput.vue';
-import BaseButton from '@/component/BaseButton.vue';
+import BookSearchBar from '@/component/BookSearchBar.vue';
 </script>
 
 <template>
   <MainLayout>
     <main>
-      <base-input prefix-icon="search" placeholder="hello">
-        <template #inner-right>
-          <base-button>Search</base-button>
-        </template>
-      </base-input>
+      <BookSearchBar />
     </main>
   </MainLayout>
 </template>
