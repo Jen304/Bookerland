a0c03100cd38b773bde124eac2bc23345e245aaa
Jen Hoang <jenhoang304@gmail.com>
2023-11-13 19:16:24 -0800

    Add search bar to home page

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -10,8 +10,25 @@ import BookSearchBar from '@/component/BookSearchBar.vue';
     <template #header>
       <MainHeader only-nav />
     </template>
-    <main>
-      <BookSearchBar />
+    <main class="f-w-container">
+      <section :class="['row', $style['search-bar']]">
+        <div class="col-6">
+          <img src="@/asset/logo.png" alt="logo" />
+          <BookSearchBar />
+        </div>
+      </section>
     </main>
   </MainLayout>
 </template>
+<style module>
+.search-bar img {
+  height: 5.4rem;
+  margin: auto;
+  display: block;
+  margin-bottom: calc(var(--base-spacing) * 4);
+}
+.search-bar {
+  justify-content: center;
+  margin-top: calc(var(--base-spacing) * 8);
+}
+</style>
