872efab32d47ede99109a49128100656934279ba
Jen Hoang <jenhoang304@gmail.com>
2023-12-12 17:46:08 -0800

    Add empty result state

--- src/view/SearchView.vue
+++ src/view/SearchView.vue
@@ -12,6 +12,7 @@ const props = defineProps({
 });
 const searchQuery = ref(props.query);
 const searchResult = ref([]);
+const isEmpty = ref([]);
 const isLoadingSearchResult = ref(false);
 const totalSearchResult = ref(0);
 const getSearchResult = async (searchQuery) => {
@@ -26,6 +27,7 @@ const getSearchResult = async (searchQuery) => {
   searchResult.value = data.items;
   totalSearchResult.value = data.totalItems;
   isLoadingSearchResult.value = false;
+  isEmpty.value = data.totalItems === 0;
 };
 onMounted(() => {
   getSearchResult(searchQuery);
@@ -63,14 +65,22 @@ const bookDetailValue = ref(null);
           },
         ]"
       >
-        <MainLoadingBar v-if="isLoadingSearchResult" />
-        <div v-else>
-          <h1 class="body-text-large text-semi-bold">About {{ totalSearchResult }} books found</h1>
+        <div>
+          <!-- Search result larger than 0, and isLoadingSearchResult is False -->
+          <h1 class="body-text-large text-semi-bold" v-if="totalSearchResult > 0">
+            About {{ totalSearchResult }} books found
+          </h1>
           <ul :class="['no-list-style d-flex', $style['book-list']]">
             <li v-for="book in searchResult" :key="book.id">
               <BookCard :value="book" @click="openBookDetail(book)" />
             </li>
           </ul>
+          <MainLoadingBar v-if="isLoadingSearchResult" />
+        </div>
+        <div :class="$style['empty-result']" v-if="isEmpty">
+          <img src="@/asset/book-thinking.png" alt="empty result" />
+          <h1>Sorry! No result found :(</h1>
+          <p class="sub-heading-1">Please try another keyword</p>
         </div>
       </div>
       <div class="col-7" v-show="isBookDetailOpen">
@@ -88,5 +98,21 @@ const bookDetailValue = ref(null);
   gap: calc(var(--base-spacing) * 3);
   flex-direction: column;
   margin-block: calc(var(--base-spacing) * 2);
+}
+.book-list .empty-result {
+  margin: auto;
+  text-align: center;
+  margin: auto;
+}
+.empty-result img {
+  max-width: 48rem;
+  margin: auto;
+  display: block;
+  margin-block: calc(var(--base-spacing) * 2);
+}
+.empty-result h1,
+.empty-result p {
+  margin: auto;
+  text-align: center;
 }
 </style>
